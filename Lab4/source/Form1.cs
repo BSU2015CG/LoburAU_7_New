4d24911856a8069c7c8d793cf497ccbe01e6b139
LoburAlexander <wotar95@gmail.com>
2015-05-04 00:23:16 +0300

    Лабораторная работа №4

--- Lab4/source/Form1.cs
+++ Lab4/source/Form1.cs
@@ -449,33 +449,3 @@ namespace Graphics_Lab4
 
     }
 }
-
-
-
-
-
-/*public static Bitmap ImageToMonotone(Bitmap original)
-       {
-           Bitmap newBitmap = new Bitmap(original.Width, original.Height);
-           Graphics g = Graphics.FromImage(newBitmap);
-
-           ColorMatrix colorMatrix = new ColorMatrix(
-              new float[][] 
-              {
-                 new float[] {.3f, .3f, .3f, 0, 0},
-                 new float[] {.59f, .59f, .59f, 0, 0},
-                 new float[] {.11f, .11f, .11f, 0, 0},
-                 new float[] {0, 0, 0, 1, 0},
-                 new float[] {0, 0, 0, 0, 1}
-              });
-
-           ImageAttributes attributes = new ImageAttributes();
-
-           attributes.SetColorMatrix(colorMatrix);
-
-           g.DrawImage(original, new Rectangle(0, 0, original.Width, original.Height),
-              0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attributes);
-
-           g.Dispose();
-           return newBitmap;
-       }*/
